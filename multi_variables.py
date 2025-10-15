d3df35bd57279f86671ca4eda10089f802657caf
anish030803 <76431395+anish030803@users.noreply.github.com>
2025-10-15 18:03:07 -0400

    Add files via upload

---
+++ multi_variables.py
@@ -0,0 +1,8 @@
+# a,b,c=("Orange","Banana","Mango")
+# print(a)
+# print(b)
+# print(c)
+a=b=c=("Orange")
+print(a)
+print(b)
+print(c)
---
+++ variables.py
@@ -0,0 +1,18 @@
+# x=5
+# y="Anish"
+# print(x)
+# print(y)
+
+# x=7
+# x=10
+# print(x)
+
+# x=(5)
+# y="John"
+# print(type(x))
+# print(type(y))
+
+a="Anish"
+A="Rakesh"
+print(a)
+print(A)
